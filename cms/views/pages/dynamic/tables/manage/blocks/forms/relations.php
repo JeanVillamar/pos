6da76b4f3dee6821565c68b6ba44b72b935fb731
Jean Villamar <jfvillam@espol.edu.ec>
2025-03-16 23:18:51 -0500

    Refactor la estructura del formulario de relaciones: mejorar la legibilidad del código y optimizar la lógica de selección de datos según el rol del usuario logeado.

--- cms/views/pages/dynamic/tables/manage/blocks/forms/relations.php
+++ cms/views/pages/dynamic/tables/manage/blocks/forms/relations.php
@@ -1,6 +1,6 @@
 <?php if ($module->columns[$i]->type_column == "relations"): ?>
 
-	<?php 
+	<?php
 
 	/*=============================================
 	Traemos todas las tablas
@@ -11,9 +11,9 @@
 
 	?>
 
-	<select 
-	class="form-select rounded mb-3 select2 changeRelations"
-	idColumn="<?php echo $module->columns[$i]->id_column ?>">
+	<select
+		class="form-select rounded mb-3 select2 changeRelations"
+		idColumn="<?php echo $module->columns[$i]->id_column ?>">
 
 		<?php if ($module->columns[$i]->matrix_column != null): ?>
 
@@ -26,55 +26,95 @@
 
 		<?php endif	?>
 
-			<?php foreach ($tables as $index => $item): ?>
+		<?php foreach ($tables as $index => $item): ?>
 
-				<option value="<?php echo $item ?>" <?php if (!empty($data) && $module->columns[$i]->matrix_column == $item): ?> selected <?php endif ?> ><?php echo $item ?></option>
-				
-			<?php endforeach ?>
+			<option value="<?php echo $item ?>" <?php if (!empty($data) && $module->columns[$i]->matrix_column == $item): ?> selected <?php endif ?>><?php echo $item ?></option>
+
+		<?php endforeach ?>
 
 
 	</select>
 
 	<div class="mb-3"></div>
 
-	<select 
-	class="form-select rounded select2 selectRelations"
-	name="<?php echo $module->columns[$i]->title_column ?>" 
-	id="<?php echo $module->columns[$i]->title_column ?>">
+	<select
+		class="form-select rounded select2 selectRelations"
+		name="<?php echo $module->columns[$i]->title_column ?>"
+		id="<?php echo $module->columns[$i]->title_column ?>">
 
-	<?php if ($module->columns[$i]->matrix_column != null): ?>
+		<?php if ($module->columns[$i]->matrix_column != null): ?>
 
-		<?php 
+			<?php
 
-			$url = $module->columns[$i]->matrix_column;
+
+
+			echo '<script>console.log(' . json_encode($_SESSION) . ');</script>';
+
+			//filramos las relaciones que se hagan con la tabla admin y sucursales
+			//pero antes se debe filtrar si es el superadmin, el admin o un vendedor que solo deben tener
+			//acceso a su sucursal asignada
+			if ($_SESSION['admin']->rol_admin == 'superadmin' || ($_SESSION['admin']->rol_admin == 'admin' && !isset($_SESSION['admin']->phone_office))) {
+				$url = $module->columns[$i]->matrix_column; //como es el superadmin y un admin general se obtendra la data de todas las sucursales y usuarios
+
+
+
+			} else {
+				$idAdmin = $_SESSION['admin']->id_admin;
+				//si es un usuario de una sucursal se obtendra la data de la sucursal a la que pertenece
+				// echo '<script>';
+				// echo 'console.log('.json_encode($module).');';
+				// echo '</script>';
+				$idOffice = $_SESSION['admin']->id_office_admin;
+				$nombre_tabla = $module->columns[$i]->matrix_column;
+				//{{endpoint}}admins?select=*&linkTo=id_admin&equalTo=3
+				$url = $nombre_tabla
+					. '?select=*&linkTo=id_' . substr($nombre_tabla, 0, -1)
+					. '&equalTo=' . ${'id' . ucfirst(substr($nombre_tabla, 0, -1))};
+				// echo '<script>console.log(' . json_encode($url) . ');</script>';
+			}
+
+
 			$method = "GET";
 			$fields = array();
 
-			$columnsTable = CurlController::request($url,$method,$fields);
+			$columnsTable = CurlController::request($url, $method, $fields);
 
-			if($columnsTable->status == 200){
+			if ($columnsTable->status == 200) {
 
 				$columnsTable = $columnsTable->results;
 
-			}else{
+				echo '<script>console.log(' . json_encode($columnsTable) . ');</script>';
+			} else {
 
 				$columnsTable = array();
 			}
 
-		?>
+			?>
 
-		<?php if (!empty($columnsTable)): ?>
+			<?php if (!empty($columnsTable)): ?>
 
-			<?php foreach ($columnsTable as $index => $item): ?>
 
-				<option value="<?php echo json_decode(json_encode($item),true)[array_keys((array)$item)[0]] ?>" <?php if (!empty($data) && json_decode(json_encode($item),true)[array_keys((array)$item)[0]] == $data[$module->columns[$i]->title_column]): ?> selected <?php endif ?>><?php echo json_decode(json_encode($item),true)[array_keys((array)$item)[0]] ?> - <?php echo urldecode(json_decode(json_encode($item),true)[array_keys((array)$item)[1]]) ?></option>
+				<!-- Recorremos los resultados para mostrarlos como opciones en el select -->
+				<?php foreach ($columnsTable as $index => $item): ?>
 
-			<?php endforeach ?>
+					<?php
+					// Convertimos el item en un array para acceder fácilmente a sus claves y valores
+					$itemArray = json_decode(json_encode($item), true);
+					$firstKey = array_keys($itemArray)[0];
+					$secondKey = array_keys($itemArray)[1];
+					?>
+
+					<option value="<?php echo $itemArray[$firstKey] ?>"
+						<?php if (!empty($data) && $itemArray[$firstKey] == $data[$module->columns[$i]->title_column]): ?> selected <?php endif ?>>
+						<?php echo $itemArray[$firstKey] ?> - <?php echo urldecode($itemArray[$secondKey]) ?>
+					</option>
+
+				<?php endforeach ?>
+
+			<?php endif ?>
 
 		<?php endif ?>
 
-	<?php endif ?>
-		
 
 	</select>
 
